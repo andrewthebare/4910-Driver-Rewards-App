1c8ce390d10c03b7cc92b76a4071b472d8f97655
Andrew Bare <andrewebare21@yahoo.com>
2021-09-16 00:01:10 -0400

    Basic put to the DB, button hardcoded adds Fred

--- driver-app-frontend/src/pages/CreateUser.jsx
+++ driver-app-frontend/src/pages/CreateUser.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import axios from 'axios';
 
 export default function CreateUser(){
 
+  const onFormSubmit = () =>{
+    //make sure that the stuff is ok
+    console.log('Hi');
+
+    const newUser = { 
+      firstName: 'Fred',
+      lastName: 'Flinstone'
+    };
+
+    //post it to the server
+    axios.post('http://localhost:8081/newUser', newUser)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    console.log('Hello');
+
+  }
+
   return(
     <div>
-      <form>
+      <form onSubmit={()=>onFormSubmit}>
         <label htmlFor="FirstName">First Name</label>
         <input id='FirstName' type='text'/>
         <label htmlFor="LastName">Last Name</label>
@@ -12,6 +35,7 @@ export default function CreateUser(){
 
         <button type='submit'>Submit</button>
       </form>
+      <button onClick={onFormSubmit}>Add Fred Flinstone</button>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ var server = app.listen(8081, function () {
   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
 })
+app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',function (req,res){
 
@@ -28,5 +34,28 @@ app.get('/',function (req,res){
   });
 
   // console.log(req);
-  res.send(404);
+  res.send(200);
+})
+
+app.post('/newUser',function(req,res){
+  console.log('Someone is posting a new user!');
+  console.log(req.body);
+
+  var con = mysql.createConnection({
+    host: "db-prod.cjpjh4cuj9z5.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "Team3Test",
+    database: "sys"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("insert into users(FirstName, LastName) values('Fred','Flinstone')", function (err, result, fields) {
+      if (err) throw err;
+      console.log('result', result);
+    });
+  });
+
+  res.send({'response':'Thanks'}).status(200);
+
 })
