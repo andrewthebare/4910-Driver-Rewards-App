19887c38c81a9d30a8c21dc5128940e7df4e8f50
Andrew Bare <andrewebare21@yahoo.com>
2021-11-04 01:20:34 -0400

    Reusable dashboard Card, Admin Dashboard designed

--- driver-app-frontend/src/pages/AdminDashboard.jsx
+++ driver-app-frontend/src/pages/AdminDashboard.jsx
@@ -8,45 +8,13 @@ export default function AdminDashboard(){
     <div>
       <h1> Administrator Dashboard </h1>
 
-      <div className='gridHolder'>
-        <DashboardCard src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F1%2F19%2F%25C3%2581guila_calva.jpg&f=1&nofb=1' to="/adminDashboard/createUser"/>
-        <div className='dashHolder reverse'>
-          <div className='content'>This is content</div>
-          <button className= "btn2" >
-            <Link class="adminButton" to="/adminDashboard/editUser">
-              Edit User
-              <span class="sr-only">(current)</span>
-            </Link>
-          </button>
-        </div>
+      <div className='dashHolder'>
+        <DashboardCard text='Create User' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.gettyimages.com%2Fphotos%2Fhandshake-picture-id487252293%3Fb%3D1%26k%3D6%26m%3D487252293%26s%3D612x612%26w%3D0%26h%3DLiKW3FD5zo1I81qCE2M5twalu1nfxUIG1VfhPOzVg6w%3D&f=1&nofb=1' to="/adminDashboard/createUser"/>
+        <DashboardCard text='Manage Users' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F3874031%2Fpexels-photo-3874031.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-gustavo-fring-3874031.jpg%26fm%3Djpg&f=1&nofb=1' to="/adminDashboard/editUser"/>
+        <DashboardCard text='View Logs' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.goodfreephotos.com%2Fcache%2Fother-photos%2Fhanging-clock-on-building.jpg&f=1&nofb=1' to="/adminDashboard/Logs"/>
+        <DashboardCard text='Manage Catalog' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F972887%2Fpexels-photo-972887.jpeg%3Fcs%3Dsrgb%26dl%3Dadult-blur-caucasian-972887.jpg%26fm%3Djpg&f=1&nofb=1' to="/adminDashboard/Logs"/>
+        <DashboardCard text='View Sponsor' src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.publicdomainpictures.net%2Fpictures%2F150000%2Fvelka%2Ftruck-on-the-road.jpg&f=1&nofb=1' to="/adminDashboard/Logs"/>
       </div>
-
-      <center>
-            <button className= "btn2">
-              <Link class="adminButton" to="/adminDashboard/Logs">
-                View Logs
-              <span class="sr-only">(current)</span>
-              </Link>
-            </button>
-      </center>
-
-      <center>
-            <button className= "btn2">
-              <Link class="adminButton" to="/adminDashboard/manageCatalog">              
-                Catalog
-              <span class="sr-only">(current)</span>
-              </Link>
-            </button>
-      </center>
-
-      <screenLeft>
-          <button className= "btn2">
-            <Link class="adminButton" to="/Error">
-              Placeholder
-            <span class="sr-only">(current)</span>
-            </Link>
-          </button>
-      </screenLeft>
     </div>
   )
 }
--- driver-app-frontend/src/pages/components/DashboardCard.jsx
+++ driver-app-frontend/src/pages/components/DashboardCard.jsx
@@ -4,18 +4,17 @@ import { Link, withRouter } from "react-router-dom";
 export default function DashboardCard(props) {
   
   return(
-    <div className='dashCard neomorph'>
+    <div className='neomorph dashCard'>
 
-      <img src={props.src} style={{maxWidth:'100%', height:'auto', borderRadius:'8px'}}/>
+      <img className='' src={props.src} style={{maxWidth:'100%', height:'auto', border:'1pt solid #aaa', borderRadius:'8px'}}/>
       <div className='content'>
-        Hello from Cards this is very long to be annoying
+        <p>{props.description}</p>
       </div>
 
       <div className='btnHolder'>
-        <button className= "btn2">
+        <button className= "dashbtn neoInverse">
           <Link class="adminButton" to={props.to}>
-            Create User
-            <span class="sr-only">(current)</span>
+            {props.text}
           </Link>
         </button>
       </div>
